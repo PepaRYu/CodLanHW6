cc46a2258123aef332e0f570af0c1415f30e152e
DrPRun <sheighd@gmail.com>
2022-08-10 21:41:30 +0300

    Zadacha 043

---
+++ Zadacha043/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void crossLines(double k1, double b1, double k2, double b2){
+    double xCross = 0;
+    double yCross = 0;
+    if (k1 != k2){
+        xCross = (b2 - b1)/(k1 - k2);
+        yCross = k1 * xCross + b1;
+        Console.WriteLine($"Координаты точки пересечения прямых: ({xCross}, {yCross})");
+    }
+    else{
+        Console.WriteLine("Прямые не пересекаются");
+    }
+
+}
+
+
+
+Console.WriteLine("Введите коэфициент наклона первой прямой k1");
+double k1 =Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите свободный коэффициен первой прямой b1");
+double b1 =Convert.ToDouble(Console.ReadLine());
+
+
+
+
+Console.WriteLine("Введите коэфициент наклона первой прямой k2");
+double k2 =Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите свободный коэффициен первой прямой b2");
+double b2 =Convert.ToDouble (Console.ReadLine());
+
+
+crossLines(k1, b1, k2, b2);
